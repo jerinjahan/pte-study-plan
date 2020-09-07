1cb550ec431397ce3b2916e06ebbbd0e9ffe0441
Jerin Jahan <crytekredhat@gmail.com>
2020-09-07 19:48:31 +0600

    check test route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RoutineComponent } from './routine/routine.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RoutineComponent, pathMatch: 'full' },
+  { path: 'test-path', component: ShippingComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
